529a41b77ff96096c73cfe584a7d67a73b550344
Carlos Lavieri @cdlc0410 <carlos_lavieri@outlook.es.com>
2026-03-18 00:47:11 -0400

    version 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+import { src, dest, watch } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+export function css( done ) {
+
+    console.clear();
+
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError ) )
+        .pipe( dest('build/css') );
+    
+    
+    done();
+};
+
+export function dev() {
+    console.clear()
+    watch( 'src/scss/**/*.scss', css )
+}
