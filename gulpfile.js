dbca06a547cddc85e490c254e263b7aa29f4dc0f
cdlc0410 <carlos_lavieri@outlook.es.com>
2026-03-18 11:39:25 -0400

    agregando JS al proyecto, ajuste del gulpfile para generar el JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
+
+export function js( done ) {
+
+    return src('src/js/app.js')
+        .pipe( dest('build/js') );
+}
 
 export function css( done ) {
 
@@ -13,7 +19,8 @@ export function css( done ) {
 
 export function dev() {
 
-    watch( 'src/scss/**/*.scss', css )
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/js/**/*.js', js );
 }
 
-export const build = css;
+export default series( js, css, dev );
