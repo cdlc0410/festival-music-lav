8620d371e456b328977e29d940f129cf085dcd22
cdlc0410 <carlos_lavieri@outlook.es.com>
2026-03-19 08:56:16 -0400

    feat: se agrego al gulpfile la minificacion del css y el js con las dependencias de sass y gulp-terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import gulpTerser from 'gulp-terser';
 
 const sass = gulpSass(dartSass);
 
 export function js( done ) {
 
     return src('src/js/app.js')
+        .pipe( gulpTerser() )
         .pipe( dest('build/js') );
 }
 
 export function css( done ) {
 
     return src('src/scss/app.scss', {sourcemaps: true})
-        .pipe( sass().on('error', sass.logError ) )
+        .pipe( sass({
+            style: 'compressed'
+        }).on('error', sass.logError ) )
         .pipe( dest('build/css', {sourcemaps: '.'}) );
 };
 
